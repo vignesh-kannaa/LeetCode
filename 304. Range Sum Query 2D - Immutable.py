c0dee6d39b7745e84bd08ba600dec6e4e1c505f9
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-08-05 10:16:37 +0100

    arrays

---
+++ 304. Range Sum Query 2D - Immutable.py
@@ -0,0 +1,53 @@
+"""Given a 2D matrix matrix, handle multiple queries of the following type:
+
+Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+Implement the NumMatrix class:
+
+NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+You must design an algorithm where sumRegion works on O(1) time complexity.
+
+Input
+["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+[[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+Output
+[null, 8, 11, 12]
+
+Explanation
+NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
+numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
+numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green rectangle)
+numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue rectangle)
+"""
+
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        ROWS, COLS = len(matrix), len(matrix[0])
+        self.sumMat = [[0] * (COLS+1) for i in range(ROWS+1)]
+        for r in range(ROWS):
+            prefix = 0
+            for c in range(COLS):
+                prefix += matrix[r][c]
+                above = self.sumMat[r][c+1]
+                self.sumMat[r+1][c+1] = prefix + above
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        bottomRight = self.sumMat[row2+1][col2+1]
+        topRight = self.sumMat[row1][col2+1]
+        leftBottom = self.sumMat[row2+1][col1]
+        topLeft = self.sumMat[row1][col1]
+        sumval = bottomRight - topRight - leftBottom + topLeft
+        return sumval
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
+"""
+Sol: 
+Calculate curposition val such that it holds all the above rectagle box values.
+to handle edge case add extra row & col. 
+to find the sol remove the extra box values from the last box. 
+note: hanlde offset in newMatrix"""
