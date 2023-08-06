3a83cf83cc3ba67523a2e485f8a62351fb43abae
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-08-06 18:01:05 +0100

    linkedlist

---
+++ 142. Linked List Cycle II.py
@@ -0,0 +1,48 @@
+"""Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                break
+        if not fast or fast.next is None:
+            return None
+        slow2 = head
+        while slow != slow2:
+            slow = slow.next
+            slow2 = slow2.next
+        return slow
+
+
+"""
+sol:
+use slow and fast pointer
+if slow and fast pointer meets then there is a cycle
+if the fast pointer ends, no cycle
+the new slow pointer initialised at the time of slow= fast will meet the slow pointer at the head (fact)
+"""
