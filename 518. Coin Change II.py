ed765fdd864b8741d4e40deb22785296a9ee5c56
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-08-11 13:47:53 +0100

    dp

---
+++ 518. Coin Change II.py
@@ -0,0 +1,53 @@
+"""You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+You may assume that you have an infinite number of each kind of coin.
+
+The answer is guaranteed to fit into a signed 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+Example 2:
+
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
+Example 3:
+
+Input: amount = 10, coins = [10]
+Output: 1"""
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        mem = {}
+
+        def dfs(i, curamt):
+            if i >= len(coins):
+                return 0
+            if curamt == amount:
+                return 1
+            if (i, curamt) in mem:
+                return mem[(i, curamt)]
+            if curamt > amount:
+                return 0
+            if curamt + coins[i] <= amount:
+                dfs(i, curamt + coins[i])
+
+            mem[(i, curamt)] = dfs(i, curamt + coins[i]) + dfs(i+1, curamt)
+            return mem[(i, curamt)]
+        return dfs(0, 0)
+
+
+"""sol
+using unbounded knapsack"""
