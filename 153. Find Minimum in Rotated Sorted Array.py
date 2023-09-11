3deace120dc4e94175d52f8b99efc722b69d225c
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-09-11 09:19:27 +0100

    binary search

---
+++ 153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,50 @@
+"""Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. """
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        L, R = 0, len(nums)-1
+        res = float('inf')
+        while L <= R:
+            if nums[L] <= nums[R]:  # sorted
+                res = min(res, nums[L])
+                return res
+            mid = (L+R)//2
+            res = min(res, nums[mid])
+            if nums[mid] >= nums[L]:
+                L = mid+1
+            else:
+                R = mid - 1
+        return res
+
+
+"""SOL: Binary search
+1. sorted: return left most
+2. rotated: middle element is in left sorted => search in right else left
+note: add mid value to the result"""
