12fe5d1d410b2b991926cd4741f2cec9530ca12b
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-09-11 12:19:27 +0100

    linkedlist

---
+++ 143. Reorder List.py
@@ -0,0 +1,47 @@
+"""You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        slow.next = None  # assign first half end to none
+        prev = None
+        while second:
+            temp = second.next
+            second.next, prev = prev, second
+            second = temp
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+
+
+"""SOL:
+
+1. split into first and second half
+2. reverse the second half
+3. merge these both"""
