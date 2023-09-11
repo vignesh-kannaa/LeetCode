4e9c6b01709909a8437ae3856702578234e088b5
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-09-11 12:47:24 +0100

    linkedlist

---
+++ 19. Remove Nth Node From End of List.py
@@ -0,0 +1,42 @@
+"""Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        while n > 0:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next
+        return dummy.next
+
+
+"""SOL:
+use two pointers.
+1. move right pointer to given offset
+2. move left pointer to one position before= > dummy pointer
+"""
