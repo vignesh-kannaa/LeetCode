88d79edfe2a52fab39490d53fd8a9a6848564acc
Vignesh Kannaa <vigneshkannaa1996@gmail.com>
2023-09-14 17:18:05 +0100

    dp

---
+++ 300. Longest Increasing Subsequence.py
@@ -0,0 +1,31 @@
+"""Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1+LIS[j])
+        return max(LIS)
+        """get max of lis of current index or 1+next index
+        max(LIS(i), 1+ LIS(j))"""
